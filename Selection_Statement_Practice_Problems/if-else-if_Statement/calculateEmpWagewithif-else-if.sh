2d0ae13bcfcb77ff0c1b67dc98d06ac201f3d12b
surekhakallutla <surekhakallutla@gmail.com>
2021-12-01 16:00:06 +0530

    New File

---
+++ Selection_Statement_Practice_Problems/if-else-if_Statement/calculateEmpWagewithif-else-if.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+ABSENT= 0;
+PRESENT=1;
+PART_TIME=2;
+PAY_PER_HOUR=20;
+
+empCheck=$((RANDOM%3));
+
+if [ $empCheck -eq $PRESENT ]
+then
+		echo "Employee is present";
+		empHr=8;
+	elif [ $empCheck -eq $PART_TIME ]
+	then 
+		echo "Employee is working part time";
+		empHr=4;
+	elif [ $empCheck -eq $ABSENT ]
+	then
+		echo "Employee is absent";
+		empHr=0;
+fi
+
+salary=$(($PAY_PER_HOUR * $empHr));
+
+echo "Employee salary : $salary";
